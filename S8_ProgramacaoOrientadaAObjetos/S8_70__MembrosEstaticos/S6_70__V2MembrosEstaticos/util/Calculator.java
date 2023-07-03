06e1c31c6cb1b3e7abe35d51d5f1abda5905af9e
rheydne <rheydne@gmail.com>
2023-07-02 23:47:11 -0300

    Iniciacao A70 Membros Estaticos

---
+++ S8_ProgramacaoOrientadaAObjetos/S8_70__MembrosEstaticos/S6_70__V2MembrosEstaticos/util/Calculator.java
@@ -0,0 +1,14 @@
+package S8_ProgramacaoOrientadaAObjetos.S8_70__MembrosEstaticos.S6_70__V2MembrosEstaticos.util;
+
+public class Calculator {
+
+    public final double PI = 3.14159;
+
+    public double circumference(double radius) {
+        return 2 * PI * radius;
+    }
+
+    public double volume(double radius) {
+        return 4 * PI * radius * radius * radius / 3;
+    }
+}
