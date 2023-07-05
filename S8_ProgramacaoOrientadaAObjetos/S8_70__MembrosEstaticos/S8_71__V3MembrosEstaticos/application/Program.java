4765e1d820cad3327bb6db39c99fc824db1832cb
rheydne <rheydne@gmail.com>
2023-07-05 14:29:09 -0300

    Membros Estaticos V3

---
+++ S8_ProgramacaoOrientadaAObjetos/S8_70__MembrosEstaticos/S8_71__V3MembrosEstaticos/application/Program.java
@@ -0,0 +1,31 @@
+package S8_ProgramacaoOrientadaAObjetos.S8_70__MembrosEstaticos.S8_71__V3MembrosEstaticos.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import S8_ProgramacaoOrientadaAObjetos.S8_70__MembrosEstaticos.S8_71__V3MembrosEstaticos.util.Calculator;
+
+public class Program {
+    public static void main(String[] args) {
+
+        // Exemplo da classe sendo instanciada, sem a utilização de membros estaticos
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double radius, c, v;
+
+        System.out.print("Enter radius: ");
+        radius = sc.nextDouble();
+
+        c = Calculator.circumference(radius);
+        
+        v = Calculator.volume(radius);
+
+        System.out.printf("Circunference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
+
+        sc.close();
+    }
+}
---
+++ S8_ProgramacaoOrientadaAObjetos/S8_70__MembrosEstaticos/S8_71__V3MembrosEstaticos/util/Calculator.java
@@ -0,0 +1,14 @@
+package S8_ProgramacaoOrientadaAObjetos.S8_70__MembrosEstaticos.S8_71__V3MembrosEstaticos.util;
+
+public class Calculator {
+
+    public static final double PI = 3.14159;
+
+    public static double circumference(double radius) {
+        return 2 * PI * radius;
+    }
+
+    public static double volume(double radius) {
+        return 4 * PI * radius * radius * radius / 3;
+    }
+}
