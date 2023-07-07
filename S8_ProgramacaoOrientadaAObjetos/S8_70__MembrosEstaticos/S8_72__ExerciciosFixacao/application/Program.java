4c068c9a78d9cc3bff5ca5960b78575433ef215b
rheydne <rheydne@gmail.com>
2023-07-07 19:07:53 -0300

    Membros Estaticos Ex 3

---
+++ S8_ProgramacaoOrientadaAObjetos/S8_70__MembrosEstaticos/S8_72__ExerciciosFixacao/application/Program.java
@@ -0,0 +1,31 @@
+package S8_ProgramacaoOrientadaAObjetos.S8_70__MembrosEstaticos.S8_72__ExerciciosFixacao.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import S8_ProgramacaoOrientadaAObjetos.S8_70__MembrosEstaticos.S8_72__ExerciciosFixacao.util.Dolar;
+
+public class Program {
+    
+    public static void main(String[] args) {
+        
+        // Faca um programa para ler a cotacao do dolar, e depois um valor em dolares para
+        // ser comprado por uma pessoa em reais. Informar quantos reais vai pagar pelos
+        // dolares, considerando ainda que a pessoa tera de pagar 6% de IOF sobre o valor
+        // em dolar
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double currentQuote, buyDollar;
+
+        System.out.print("What is the dolar price: ");
+        currentQuote = sc.nextDouble();
+        System.out.print("How many dollars will be bought: ");
+        buyDollar = sc.nextDouble();
+
+        System.out.printf("Amount to be paid in reais: %.2f", Dolar.CurrencyConverter(currentQuote, buyDollar));
+        
+        sc.close();
+    }
+}
---
+++ S8_ProgramacaoOrientadaAObjetos/S8_70__MembrosEstaticos/S8_72__ExerciciosFixacao/util/Dolar.java
@@ -0,0 +1,10 @@
+package S8_ProgramacaoOrientadaAObjetos.S8_70__MembrosEstaticos.S8_72__ExerciciosFixacao.util;
+
+public class Dolar {
+    
+    private static final double tax = 6;
+
+    public static double CurrencyConverter(double currentQuote, double buyDollar) {
+        return buyDollar * currentQuote * ((tax / 100) + 1);
+    }
+}
